289264954f871cbb4397bb2b336ceca11f2d8067
Javier Horacio Fuchs <javier.horacio.fuchs@icloud.com>
2023-08-31 20:51:50 -0300

    Handy App net 7

---
+++ HandyAppNet7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HandyAppNet7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ HandyAppNet7/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace HandyAppNet7;
+
+public partial class MainPage : ContentPage
+{
+	int count = HandyLibNet7.Helper.Diga33();
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
+
